2e21140a5e39d70b01fca6913132de9b32246bd9
Bablushejul <shejulbablu@gmail.com>
2023-02-21 16:48:34 +0530

    button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,9 @@
 import ExpenseDate from './ExpepenseDate';
 import './ExpenseItem.css'
 function ExpenseItem(props) {
- 
+ const clickHandler=()=>{
+  console.log("clicked!!!");
+ }
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
@@ -9,6 +11,7 @@ function ExpenseItem(props) {
         <h2>{props.tittle}</h2>
         </div>
         <div className='expense-item_price'>${props.amount}</div>
+        <button onClick={clickHandler}>Change</button>
       
     </div>
   );
